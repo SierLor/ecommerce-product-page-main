ab13942b4cfb7b776a4b08b17cf91f62baf08a6f
Loraweb <95880723+SierLor@users.noreply.github.com>
2024-11-06 20:07:48 +0100

    html y css terminado

---
+++ main.js
@@ -0,0 +1,10 @@
+// HACER APARECER EL MODAL HACIENDO CLICK EN EL MENU HAMBURGUESA
+
+const hamburguesa = document.querySelector(".header__hamburguer");
+const modalHamburguesa = document.querySelector(".header__background");
+const close = document.querySelector(".header__close");
+
+
+hamburguesa.addEventListener("click", ()=>{
+    modalHamburguesa.classList.toggle
+})
